3b1a9b131a54b1eb35d311d85252de8a28838816
emilienmottet-bib <emilien.mottet@michelin.com>
2020-03-03 13:54:46 +0100

    V0.01

--- partialmixformat.nim
+++ partialmixformat.nim
@@ -1,4 +1,4 @@
-import os, osproc, tempfile, typetraits, strutils
+import os, osproc, tempfile, typetraits, strutils, sets
 import nre except toSeq
 
 type
@@ -10,43 +10,57 @@ type
   Diff = tuple
     prev: FilePosition
     new: FilePosition
+    txt: string
 
-proc amended_lines(diff_file :string): seq[Diff] =
+proc amended_lines(diff_file: string): seq[Diff] =
   let diff_content  = readFile(diff_file)
   var res: seq[Diff] = @[]
-  var diff_sequence = diff_content.split(re"@@")
-  echo diff_sequence
+  var diff_sequence = diff_content.split(re"(?m)^@@")
   diff_sequence.delete(0)
   for diff_block in diff_sequence:
-    echo diff_block
     let captures = diff_block.find(re"-([0-9]+),?([0-9]*) \+([0-9]+),?([0-9]*)").get.captures()
     let prev_size = if captures[1] == "" : 0
                     else: captures[1].parseInt()
     let next_size = if captures[3] == "" : 0
                 else: captures[3].parseInt()
-    res.add(((captures[0].parseInt(),prev_size),(captures[2].parseInt(),next_size)))
+    res.add(((captures[0].parseInt(),prev_size),(captures[2].parseInt(),next_size), "@@" & diff_block))
   return res
 
 
-# proc match_diff()
+proc match_diff(current: seq[Diff], new: seq[Diff]): seq[Diff] =
+  var res: seq[Diff] = @[]
+  var i=0
+  var j=0
+  while (i < current.len()) and (j < new.len()):
+    if current[i].new.start > new[j].prev.start + new[j].prev.size:
+      inc j
+      continue
+    if current[i].new.start + current[i].new.size < new[j].prev.start :
+      inc i
+      continue
+    res.add(new[j])
+    inc j
+  res
 
-proc main() =
+
+
+proc main(): string  =
   if paramCount() != 1:
     quit("synopsis: " & getAppFilename() & " filename keyfield valuefield")
   let
     filename = paramStr(1)
 
   var (_ , name_tempfile_original) = mkstemp()
-  echo name_tempfile_original
+  # echo name_tempfile_original
 
   var (_ , name_tempfile_fmt) = mkstemp()
-  echo name_tempfile_fmt
+  # echo name_tempfile_fmt
 
   var (_ , diff_with_head) = mkstemp()
-  echo diff_with_head
+  # echo diff_with_head
 
   var (_ , diff_with_fmt) = mkstemp()
-  echo diff_with_fmt
+  # echo diff_with_fmt
 
   copyFile(filename,name_tempfile_original)
   copyFile(filename,name_tempfile_fmt)
@@ -57,9 +71,24 @@ proc main() =
   let errC_diff_formated = execCmd("diff -U 0 " & name_tempfile_original & " " & name_tempfile_fmt & " > " & diff_with_fmt )
 
   let indexes_diff_with_head = amended_lines(diff_with_head)
-  echo indexes_diff_with_head
+  # echo indexes_diff_with_head
 
-  # let indexes_diff_fmt = amended_lines(diff_with_fmt)
+  let indexes_diff_fmt = amended_lines(diff_with_fmt)
   # echo indexes_diff_fmt
 
-main()
+  let only_match = match_diff(indexes_diff_with_head,indexes_diff_fmt)
+  # echo only_match
+
+  var (_ , name_tempfile_final) = mkstemp()
+  # echo name_tempfile_final
+
+  var res = ""
+
+  for c in only_match:
+    res = res & c.txt
+
+  writeFile(name_tempfile_final,res)
+
+  name_tempfile_final
+
+echo main()
